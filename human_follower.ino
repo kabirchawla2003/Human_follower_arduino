c21f939a86f2af16e70105d494d3eb5e7d5a0b75
kabirchawla2003 <56606599+kabirchawla2003@users.noreply.github.com>
2022-08-16 23:31:35 +0530

    Add files via upload

--- human_follower.ino
+++ human_follower.ino
@@ -5,6 +5,8 @@ int ir1dat;
 int ir2dat;
 int ir3dat;
 
+int half_turn_time = 200;
+
 void mrun(int m11, int m12, int m21, int m22){
   digitalWrite(5,m11);
   digitalWrite(6,m12);
@@ -27,5 +29,29 @@ void loop() {
   ir1dat = digitalRead(ir1);
   ir2dat = digitalRead(ir2);
   ir3dat = digitalRead(ir3);
+  if (ir1dat == 1 & ir2dat == 0 & ir3dat == 0){
+    mrun(1,0,0,1);
+    delay(2*half_turn_time);
+    mrun(0,0,0,0);
+  }
+  else if(ir1dat == 0 & ir2dat == 0 & ir3dat == 1){
+    mrun(0,1,1,0);
+    delay(2*half_turn_time);
+    mrun(0,0,0,0);
+  }
+  else if(ir1dat == 0 & ir2dat == 1 & ir3dat == 1){
+    mrun(0,1,1,0);
+    delay(half_turn_time);
+    mrun(0,0,0,0);
+  }
+  else if(ir1dat == 1 & ir2dat == 1 & ir3dat == 0){
+    mrun(1,0,0,1);
+    delay(half_turn_time);
+    mrun(0,0,0,0);
+  }
+  else{
+    mrun(1,0,1,0);
+  }
+  delayMicroseconds(2);
 
 }
